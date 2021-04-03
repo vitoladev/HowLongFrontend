d9a9aca585b7299f26b875a3a8c7b0a7ba262a87
vitoladev <asaptorvi@protonmail.com>
2021-04-03 14:54:01 -0300

    feat: integration with API

---
+++ src/http/sportService.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const getBySport = async (sport: string) => {
+  const sportUri = process.env.REACT_APP_SPORT_URI;
+  const response = await axios.get(`${sportUri}/${sport}`);
+  const { data } = response;
+  return data;
+};
+
+const getById = async (id: string) => {
+  const teamUri = process.env.REACT_APP_TEAM_URI;
+  const response = await axios.get(`${teamUri}/${id}`);
+  const { data } = response;
+
+  return data;
+};
+
+export { getBySport, getById };
